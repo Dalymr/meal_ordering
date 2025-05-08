101d0f11e83d647b3dfe7816d834325d15b50a57
wass <sessinowassim@gmail.com>
2025-05-08 09:40:00 +0100

    about page

---
+++ public/about.php
@@ -0,0 +1,7 @@
+<?php
+// public/about.php
+include '../includes/header.php';
+?>
+<h2>À propos de FoodFrenzy</h2>
+<p>Une plateforme simple et rapide pour commander vos plats préférés.</p>
+<?php include '../includes/footer.php'; ?>
