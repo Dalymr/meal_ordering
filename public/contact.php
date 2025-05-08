6f608e747466d74ecb8346371bad1382e424b1c3
wass <sessinowassim@gmail.com>
2025-05-08 11:30:00 +0100

    contact page

---
+++ public/contact.php
@@ -0,0 +1,7 @@
+<?php
+// public/contact.php
+include '../includes/header.php';
+?>
+<h2>Contactez-nous</h2>
+<p>Pour toute question, envoyez-nous un message à support@foodfrenzy.com</p>
+<?php include '../includes/footer.php'; ?>
